f26a29b0984a950b2836c35017ea7bcb0d19de75
Maya Estalilla <mestalilla@gmail.com>
2018-03-11 05:39:54 -0400

    style: Convert tabs to spaces using a tab width of 2

--- src/App.js
+++ src/App.js
@@ -6,59 +6,59 @@ import SearchBooks from './SearchBooks'
 import './App.css'
 
 class BooksApp extends Component {
-	// Component State
-	state = {
-		books: []
-	}
+  // Component State
+  state = {
+    books: []
+  }
 
-	// Custom Methods
-	updateBook = (book, shelf) => {
-		// Update book in "database"
-		BooksAPI.update(book, shelf).then((resp) => {
-			const books = []
-			const booksById = this.state.books.reduce((obj, item) => {
-				obj[item.id] = item
-				return obj
-			}, {})
+  // Custom Methods
+  updateBook = (book, shelf) => {
+    // Update book in "database"
+    BooksAPI.update(book, shelf).then((resp) => {
+      const books = []
+      const booksById = this.state.books.reduce((obj, item) => {
+        obj[item.id] = item
+        return obj
+      }, {})
 
-			// Update state
-			for (const shelf of Object.entries(resp)) {
-				for (const bookId of shelf[1]) {
-					const newBookObj = booksById[bookId]
-					newBookObj.shelf = shelf[0]
-					books.push(newBookObj)
-				}
-			}
+      // Update state
+      for (const shelf of Object.entries(resp)) {
+        for (const bookId of shelf[1]) {
+          const newBookObj = booksById[bookId]
+          newBookObj.shelf = shelf[0]
+          books.push(newBookObj)
+        }
+      }
 
-			this.setState(books)
-		})
-	}
+      this.setState(books)
+    })
+  }
 
-	// Lifecycle Methods
-	componentDidMount() {
-		BooksAPI.getAll().then((books) => {
-			this.setState({books})
-		})
-	}
+  // Lifecycle Methods
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books})
+    })
+  }
 
-	// Render Method
-	render() {
-		return (
-			<div className="app">
-				<Route path="/" exact render={() => (
-					<ListBooks
-						books={this.state.books}
-						updateBook={(book, shelf) => {
-							this.updateBook(book, shelf)
-						}} />
-				)} />
+  // Render Method
+  render() {
+    return (
+      <div className="app">
+        <Route path="/" exact render={() => (
+          <ListBooks
+            books={this.state.books}
+            updateBook={(book, shelf) => {
+              this.updateBook(book, shelf)
+            }} />
+        )} />
 
-				<Route path="/search" render={() => (
-					<SearchBooks />
-				)} />
-			</div>
-		)
-	}
+        <Route path="/search" render={() => (
+          <SearchBooks />
+        )} />
+      </div>
+    )
+  }
 }
 
 export default BooksApp
--- src/Book.js
+++ src/Book.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
 
 class Book extends Component {
-	render() {
-		const { title, authors, imageLinks, shelf } = this.props.book
+  render() {
+    const { title, authors, imageLinks, shelf } = this.props.book
 
-		return (
-			<div className="book">
-				<div className="book-top">
-					<div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${ imageLinks.thumbnail })` }}></div>
-					<div className="book-shelf-changer">
-						<select
-							value={shelf}
-							onChange={(event) => this.props.onShelfChange(
-								this.props.book,
-								event.target.value
-							)}>
-							<option value="none" disabled>Move to...</option>
-							<option value="currentlyReading">Currently Reading</option>
-							<option value="wantToRead">Want to Read</option>
-							<option value="read">Read</option>
-							<option value="none">None</option>
-						</select>
-					</div>
-				</div>
-				<div className="book-title">{ title }</div>
-				<div className="book-authors">{ authors.join(', ') }</div>
-			</div>
-		)
-	}
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${ imageLinks.thumbnail })` }}></div>
+          <div className="book-shelf-changer">
+            <select
+              value={shelf}
+              onChange={(event) => this.props.onShelfChange(
+                this.props.book,
+                event.target.value
+              )}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{ title }</div>
+        <div className="book-authors">{ authors.join(', ') }</div>
+      </div>
+    )
+  }
 }
 
 export default Book
